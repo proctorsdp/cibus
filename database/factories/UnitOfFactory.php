fa7ea9d2d5624b08f7737c71ec88f7c212962a72
Steven <steven@MacBook-Pro.attlocal.net>
2020-06-10 19:50:32 -0400

    DB Schema  - Added System and UnitOf

---
+++ database/factories/UnitOfFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\UnitOf;
+use Faker\Generator as Faker;
+
+$factory->define(UnitOf::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_06_08_204110_create_unit_ofs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitOfsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_ofs', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_ofs');
+    }
+}
